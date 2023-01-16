ecfe4302630f43b7308ea86eb8c4a98b7791124e
Mateus Tavares <mateustavares@pop-os.localdomain>
2023-01-16 10:57:57 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/* eslint-disable indent */
+module.exports = {
+    roots: ['<rootDir>/src'],
+    collectCoverageFrom: [
+        '<rootDir>/src/**/*.{ts,tsx}'
+    ],
+    coverageDirectory: 'coverage',
+    testEnvironment: 'node',
+    transform: {
+        '.+\\.(ts|tsx)$': 'ts-jest'
+    }
+
+}
